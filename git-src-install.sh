e427310838533310262c2f596ceb85b4d697c1a1
root <root@Xenerver5.6-DDK>
2013-05-08 09:36:13 -0400

    update remote function

--- git-src-install.sh
+++ git-src-install.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
+set -x
+
 yum_install() {
     yum --enablerepo=base install -y $@
 }
 
-
+# install dependency packages
 yum_install zlib-devel curl curl-devel openssl-devel expat-devel gettext-devel autoconf automake \
             gcc make vim*
 
 # download src
 GIT_TARBALL=git-latest.tar.gz
 if [ ! -f ${GIT_TARBALL} ]; then
-    wget http://codemonkey.org.uk/projects/git-snapshots/git/${GIT_TARBALL}
+    wget -N http://codemonkey.org.uk/projects/git-snapshots/git/${GIT_TARBALL}
+else
+    echo "[WARN] ${GIT_TARBALL} is exist..."
 fi
 
 # extract it
-tar xzvf ${GIT_TARBALL}
+tar xzvf ${GIT_TARBALL} > /dev/null
 
-GIT_DIR=$(find -maxdepth 1 -name 'git-*' -type d)
+GIT_DIR=$(find . -maxdepth 1 -iname 'git-*' -type d)
+
 cd ${GIT_DIR}
 
 # generate config
@@ -30,4 +35,8 @@ make
 make install
 
 # test installation result
-git --help
+git --version
+
+# clean up
+rm -rf ${GIT_DIR}
+rm -f ${GIT_TARBALL}
